5de92a974468554c3bd654ee468fbf5dd2976d16
Alex <av.lobachev@gmail.com>
2019-05-14 16:02:22 +0300

    third

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # coding=utf-8
+import sys
+
 from var_dump import var_dump as vd
 import const
 from pprint import pprint as pp
-import reader  # CSV Files
 import os.path
 
 
@@ -15,6 +16,7 @@ class Converter:
         self.file_name = file_name
         self.file_reader = file_reader
         self.file_writer = file_writer
+        self.data = None
 
     def load_file(self):
         return self.file_reader.read(self.file_name)
@@ -22,23 +24,33 @@ class Converter:
     def save_file(self):
         self.file_writer.write(self.file_name, self.lines)
 
+    def set_data(self):
+        self.data = self.load_file()
 
-class Selector:
+    def get_data(self):
+        return self.data
+
+class FileSelector:
     '''
         Class for selecting engines
     '''
 
-    def __int__(self, file_reader, file_name):
-        self.file_reader = file_reader
-        self.file_name = file_name
+    def __init__(self, data):
         self.output_type = const.output_type
         self.input_type = const.input_type
+        self.data = data
 
-    def select(self):
-        if self.file_reader(self.file_name)[0] == self.output_type:
+    def select_header(self):
+        if self.data[0].strip() == self.output_type:
             return const.export_header_line
-        if self.file_reader(self.file_name)[0] == self.input_type:
+        if self.data[0].strip() == self.input_type:
             return const.import_header_line
+
+    def first_row(self):
+        for i, item in enumerate(self.data):
+            if item.startswith(const.start_data):
+                break
+        return i
 
 
 class FileReader:
@@ -71,7 +83,7 @@ class FileChecker:
     '''
     def check(self, file_name):
         if not os.path.exists(file_name):
-            raise IOError("File not found.")
+            sys.exit('File not found')
 
 
 def main():
